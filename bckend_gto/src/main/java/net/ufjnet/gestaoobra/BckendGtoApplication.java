52763b786bf9bde91415a2da5421582abeabaa27
Marcos Wagner de Souza Ribeiro <marcoswagnersouza@gmail.com>
2021-06-14 09:12:16 -0300

    obras

--- bckend_gto/src/main/java/net/ufjnet/gestaoobra/BckendGtoApplication.java
+++ bckend_gto/src/main/java/net/ufjnet/gestaoobra/BckendGtoApplication.java
@@ -9,7 +9,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import net.ufjnet.gestaoobra.models.Obra;
 import net.ufjnet.gestaoobra.models.Proprietario;
+import net.ufjnet.gestaoobra.repositories.ObraDAO;
 import net.ufjnet.gestaoobra.repositories.ProprietarioDAO;
 
 @EnableAutoConfiguration
@@ -20,12 +22,16 @@ public class BckendGtoApplication implements CommandLineRunner {
 	@Autowired
 	private ProprietarioDAO propDAO;
 	
+	@Autowired
+	private ObraDAO obraDAO;
+	
 	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BckendGtoApplication.class, args);
 	}
 
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -33,7 +39,15 @@ public class BckendGtoApplication implements CommandLineRunner {
 		Proprietario p2 = new Proprietario(2,"Jose","456","jose@gto");
 		Proprietario p3 = new Proprietario(3,"Maria","789","maria@gto");
 		
+		Obra o1 = new Obra(1,"Sobrado com 4 suites", "Rua Dona Olimpia, 1414, Vila Fátima","");
+		Obra o2 = new Obra(1,"Casa geminada", "Rua 15, 1515, Setor Hermosa","");
+		Obra o3 = new Obra(1,"Casa com 3 quartos", "Rua 16, 1616, Setor Brisas","");
+		Obra o4 = new Obra(1,"Casa de Alto Padrão", "Rua 17, 1717, 1414, Setor Cohacol","");
+		
+		
 		propDAO.saveAll(Arrays.asList(p1,p2,p3));
+		
+		obraDAO.saveAll(Arrays.asList(o1,o2,o3,o4));
 		
 		
 	}
