50b6d05bf42a2c50cb1d9bf3992a720f56ad2365
Marcos Wagner de Souza Ribeiro <marcoswagnersouza@gmail.com>
2021-07-23 19:54:03 -0300

    _

--- bckend_gto/src/main/java/net/ufjnet/gestaoobra/services/EnviarMailService.java
+++ bckend_gto/src/main/java/net/ufjnet/gestaoobra/services/EnviarMailService.java
@@ -1,16 +1,15 @@
 package net.ufjnet.gestaoobra.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 @Component
 public class EnviarMailService {
 	
-	
+	@Autowired
 	private JavaMailSender mailSender;
 	
 	public void enviar(String dest, String subj, String text) {
