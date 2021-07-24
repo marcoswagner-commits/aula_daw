4379cf961ed1807089fb8053ef9e94ccbba26256
Marcos Wagner de Souza Ribeiro <marcoswagnersouza@gmail.com>
2021-07-23 21:13:03 -0300

    ____

--- bckend_gto/src/main/java/net/ufjnet/gestaoobra/config/FileStorageConfig.java
+++ bckend_gto/src/main/java/net/ufjnet/gestaoobra/config/FileStorageConfig.java
@@ -1,11 +1,13 @@
 package net.ufjnet.gestaoobra.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@ConfigurationProperties(prefix= "file")
+@ConfigurationProperties(prefix= "file")
 public class FileStorageConfig {
 	
 	private String uploadDir;
--- bckend_gto/src/main/java/net/ufjnet/gestaoobra/services/FileStorageService.java
+++ bckend_gto/src/main/java/net/ufjnet/gestaoobra/services/FileStorageService.java
@@ -1,27 +1,34 @@
 package net.ufjnet.gestaoobra.services;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import net.ufjnet.gestaoobra.config.FileStorageConfig;
 import net.ufjnet.gestaoobra.services.exceptions.FileStorageException;
+import net.ufjnet.gestaoobra.services.exceptions.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
 	
-	//private final Path fileStorageLocation;
+	private final Path fileStorageLocation;
 	
 	@Autowired
 	public FileStorageService(FileStorageConfig fileStorageConfig) {
 		
-		//this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir())
-		//		.toAbsolutePath().normalize();
+		this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir())
+				.toAbsolutePath().normalize();
 		
 		try {
-			//Files.createDirectories(fileStorageLocation);
+			Files.createDirectories(fileStorageLocation);
 		} catch (Exception e) {
 			throw new FileStorageException("Não foi possível criar o diretório para o arquivo!", e);
 		}
@@ -34,8 +41,8 @@ public class FileStorageService {
 			if (fileName.contains("..")) {
 				throw new FileStorageException("Arquivo contém sequência inválida de caracteres!");
 			}
-			//Path targetLocation = fileStorageLocation.resolve(fileName);
-			//Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+			Path targetLocation = fileStorageLocation.resolve(fileName);
+			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
 			return fileName;
 		} catch (Exception e) {
 			throw new FileStorageException("Não foi possível salvar o arquivo "+fileName+". Tente novamente!", e);
@@ -44,18 +51,18 @@ public class FileStorageService {
 	
 	public Resource loadFileAsResource(String fileName) {
 		
-		//try {
-			//Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
-			//Resource resource = new UrlResource(filePath.toUri());
-			//if(resource.exists()) {
-			//	return resource;
-		//	} else {
-		//		throw new MyFileNotFoundException("Arquivo não encontrado: "+fileName+"!");
-		//	}
-		//} catch (Exception e) {
-		//	throw new MyFileNotFoundException("Arquivo não encontrado: "+fileName+"!",e);
-		//}
-		return null;
+		try {
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if(resource.exists()) {
+				return resource;
+			} else {
+				throw new MyFileNotFoundException("Arquivo não encontrado: "+fileName+"!");
+			}
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("Arquivo não encontrado: "+fileName+"!",e);
+		}
+		
 		
 	}
 	
